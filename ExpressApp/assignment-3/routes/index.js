f1ef60d7dd08cce16d6e00a5870be53322a4f40b
JanRinsma <114084427+JanRinsma@users.noreply.github.com>
2025-11-21 18:12:21 -0500

    fifth commit, main difference is the addition of a basic form for CRUD functionality

--- ExpressApp/assignment-3/routes/index.js
+++ ExpressApp/assignment-3/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET splash/home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+/* GET crud page */
+router.get('/crud', function(req, res, next) {
+  res.render('crud');
 });
 
 module.exports = router;
